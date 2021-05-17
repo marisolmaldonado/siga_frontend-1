a378923986d27ef5d16bb80f8023984a24b49188
Mauricio Tamayo <cesar.tamayo0204@gmail.com>
2021-05-17 18:34:32 -0500

    asd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,13 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const URL_API = 'http://127.0.0.1:8000/v1/';
-export const URL = 'http://127.0.0.1:8000';
+export const URL_API = 'http://siga_backend.test/v1/';
+export const URL = 'http://siga_backend.test';
 export const WEB = 'http://127.0.0.1:8000';
 
 export const environment = {
     production: false,
-    STORAGE_URL: 'http://127.0.0.1:8000/storage/',
+    STORAGE_URL: 'http://siga_backend.test/storage/',
     WEB,
     API_URL_AUTHENTICATION: URL_API + 'authentication/',
     API_URL_APP: URL_API + 'app/',
@@ -20,9 +20,7 @@ export const environment = {
     SYSTEM_ID: 1,
     CLIENT_ID: '1',
 
-    CLIENT_SECRET: 'Rc8aISE2aolIOLCXfRPY9hk1MQHfmAVbveuAcDml',
-
- 
+    CLIENT_SECRET: 'j8wVIdl8QrIhe4tfwI5ja9Bw9rjOT2oZMtFQUAxb',
     GRANT_TYPE: 'password',
     SITE_KEY: '6LcY8xAaAAAAAOTR95-UJ_zAeP9OWYPhlWg4_iFC',
 };
