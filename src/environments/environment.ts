9df5013c0cc47dca9ae535fe5eba918fb3dad403
elias9314 <kuky1993roger@gmail.com>
2021-05-17 18:03:30 -0500

    enviro

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,11 +20,10 @@ export const environment = {
 
     SYSTEM_ID: 1,
     CLIENT_ID: '1',
-<<<<<<< HEAD
+
     CLIENT_SECRET: 'Rc8aISE2aolIOLCXfRPY9hk1MQHfmAVbveuAcDml',
-=======
-    CLIENT_SECRET: 'kzhVHMVhgjAARiNcPcjBXReqJ1eRd3idKRHisUCa',
->>>>>>> mod_6_jobboard
+
+ 
     GRANT_TYPE: 'password',
 
     SITE_KEY: '6LcY8xAaAAAAAOTR95-UJ_zAeP9OWYPhlWg4_iFC',
