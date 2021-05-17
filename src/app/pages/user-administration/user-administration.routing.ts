01c58547eb18f16c79856654e56ba7b1cab94053
Pachuka <33724801+Armandot1998@users.noreply.github.com>
2021-05-16 19:40:16 -0500

    first

--- src/app/pages/user-administration/user-administration.routing.ts
+++ src/app/pages/user-administration/user-administration.routing.ts
@@ -1,11 +1,17 @@
 // Angular Router
 import {Routes} from '@angular/router';
+import {AuthGuard} from '../../shared/guards/auth.guard';
 // My Components
 
 export const UserAdministrationRouting: Routes = [
     {
         path: '',
         children: [
+            {
+                path: 'user-list',
+                loadChildren: () => import('./users-list/user-list.module').then(m => m.UserListModule),
+                canActivate: [AuthGuard]
+            },
 
         ]
     }
---
+++ src/app/pages/user-administration/users-list/user-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {TableModule} from 'primeng/table';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
