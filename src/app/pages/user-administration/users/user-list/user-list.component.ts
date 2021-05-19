dd511fe2082969f349fecfdfd924a180b524e43d
Pachuka <33724801+Armandot1998@users.noreply.github.com>
2021-05-19 12:52:33 -0500

    Adding search, painator and delete user

--- src/app/pages/user-administration/users/user-list/user-list.component.ts
+++ src/app/pages/user-administration/users/user-list/user-list.component.ts
@@ -2,6 +2,10 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from '../../../../models/auth/user';
 import { FormGroup } from '@angular/forms';
 import { Paginator } from '../../../../models/setting/paginator';
+import {MessageService} from '../../../../services/app/message.service';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {UserAdministrationService } from '../../../../services/auth/user-administration.service';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-user-list',
@@ -12,18 +16,43 @@ export class UserListComponent implements OnInit {
   @Input() usersIn: User[];
   @Input() paginatorIn: Paginator;
   @Input() formUserIn: FormGroup;
+  paginatorFiles: Paginator;
+  selectedUsers: any[];
+  selectedUser: User;
 
 
   @Output() usersOut = new EventEmitter<User[]>();
   @Output() paginator = new EventEmitter<Paginator>();
   @Output() formUserOut = new EventEmitter<FormGroup>();
   @Output() displayOut = new EventEmitter<boolean>();
+  @Output() paginatorOut = new EventEmitter<Paginator>();
 
 
-  constructor() { }
+  constructor(private messageService: MessageService,
+    private spinnerService: NgxSpinnerService,
+    private userAdministrationService: UserAdministrationService) {
+      this.resetPaginator();
+  }
 
+  resetPaginator() {
+    this.paginatorFiles = {current_page: 1, per_page: 5};
+  }
   ngOnInit(): void {
   }
+
+  searchUsers(event, search) {
+    if (event.type === 'click' || event.keyCode === 13 || search.length === 0) {
+        const params = search.length > 0 ? new HttpParams().append('search', search) : null;
+        this.spinnerService.show();
+        this.userAdministrationService.get('user-admins', params).subscribe(response => {
+            this.usersIn = response['data'],
+                this.spinnerService.hide();
+        }, error => {
+            this.spinnerService.hide();
+            this.messageService.error(error);
+        });
+    }
+}
 
   sendUsers() {
     this.usersOut.emit(this.usersIn);
@@ -38,4 +67,42 @@ export class UserListComponent implements OnInit {
     this.formUserOut.emit(this.formUserIn);
     this.displayOut.emit(true);
   }
+
+  pageChange(event) {
+    this.paginatorIn.current_page = event.page + 1;
+    this.paginatorOut.emit(this.paginatorIn);
+}
+
+  deleteUsers(skill = null) {
+    this.messageService.questionDelete({})
+        .then((result) => {
+            if (result.isConfirmed) {
+                if (skill) {
+                    this.selectedUsers = [];
+                    this.selectedUsers.push(skill);
+                }
+
+                const ids = this.selectedUsers.map(element => element.id);
+                this.spinnerService.show();
+                this.userAdministrationService.delete('user-admin/delete', ids)
+                    .subscribe(response => {
+                        this.spinnerService.hide();
+                        this.messageService.success(response);
+                        this.removeUsers(ids);
+                        this.selectedUsers = [];
+                    }, error => {
+                        this.spinnerService.hide();
+                        this.messageService.error(error);
+                    });
+            }
+        });
+
+}
+
+removeUsers(ids) {
+  for (const id of ids) {
+      this.usersIn = this.usersIn.filter(element => element.id !== id);
+  }
+  this.usersOut.emit(this.usersIn);
+}
 }
