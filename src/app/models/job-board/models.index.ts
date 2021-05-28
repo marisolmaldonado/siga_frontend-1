4a46c674b90c87cc5fbbb8f9b3adf09013a00f6c
Mauricio Tamayo <cesar.tamayo0204@gmail.com>
2021-05-28 16:44:54 -0500

    fix bugs

--- src/app/models/job-board/models.index.ts
+++ src/app/models/job-board/models.index.ts
@@ -1,10 +1,10 @@
-
-export { Reference } from './reference';
-export { Language } from './language';
-export { Experience } from './experience';
-export { Course } from './course';
-export { Category } from './category';
-export { AcademicFormation } from './academic-formation';
-export { Skill } from './skill';
-export { Company } from './company';
-export { Professional } from './professional';
+export {Reference} from './reference';
+export {Language} from './language';
+export {Experience} from './experience';
+export {Course} from './course';
+export {Category} from './category';
+export {AcademicFormation} from './academic-formation';
+export {Skill} from './skill';
+export {Company} from './company';
+export {Professional} from './professional';
+export {Offer, SearchParams} from './offer';
